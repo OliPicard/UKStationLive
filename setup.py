c94704588cef55ebf1e377a4796d5d67eb5fc649
Oliver Bryant <oliver@ufomail.co.uk>
2015-04-29 19:23:14 +0000

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='UKStationLive',
+    version='0.1',
+    packages=['requests'],
+    url='https://github.com/OliPicard/UKStationLive/',
+    license='GNU GPL 3.0',
+    author='OliPicard',
+    author_email='',
+    description=' UK Station Live Arrivals and Departures.'
+)
